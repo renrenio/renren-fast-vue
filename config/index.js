701e4cf568423674ea9b8e7dc8990ae538b06524
uustop <yth3412@163.com>
2018-03-08 09:49:20 +0800

    更改为本地端口

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
     // 代理列表, 是否开启代理通过[./dev.env.js]配置
     proxyTable: devEnv.OPEN_PROXY === false ? {} : {
       '/proxyApi': {
-        target: 'http://demo.renren.io/renren-fast/',
+        target: 'http://localhost:8080/renren-fast/',
         changeOrigin: true,
         pathRewrite: {
           '^/proxyApi': '/'
