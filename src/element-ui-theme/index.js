fb83c7d7fe68618566de18dd0f86c1cff11ef1a2
daxiongYang <daxiong.yang@qq.com>
2018-05-01 15:41:58 +0800

    主题提示

--- src/element-ui-theme/index.js
+++ src/element-ui-theme/index.js
@@ -1,6 +1,9 @@
 /**
  * element-ui组件主题
  *
+ * tips:
+ *  1. 此处只包含element-ui组件主题
+ *  2. 要达到整站主题修改效果, 请保证[active]当前主题色与[/src/assets/scss/_variables.scss]文件中[$--color-primary]属性值一致
  */
 export default {
   active: '#17b3a3', // 当前主题色
