fc25d77700e10708429069f783803ecc6b4bfd35
daxiongYang <daxiong.yang@qq.com>
2018-05-02 22:49:53 +0800

    主题调整

--- src/element-ui-theme/index.js
+++ src/element-ui-theme/index.js
@@ -3,7 +3,7 @@
  *
  * tips:
  *  1. 此处只包含element-ui组件主题
- *  2. 要达到整站主题修改效果, 请保证[import './element-[#17b3a3]/index.css']当前主题色与[/src/assets/scss/_variables.scss]文件中[$--color-primary]属性值一致
+ *  2. 要达到整站主题修改效果, 请确保[import './element-[#17b3a3]/index.css']当前主题色与[/src/assets/scss/_variables.scss]文件中[$--color-primary]属性值一致
  */
 import './element-#17b3a3/index.css' // 当前主题色
 export default {
