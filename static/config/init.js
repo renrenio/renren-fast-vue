e9d808c9ae4fdc196c9c52b147e102e175e5540d
daxiongYang <daxiong.yang@qq.com>
2018-05-06 23:52:16 +0800

    调整还原 动态同步加载初始资源

--- static/config/init.js
+++ static/config/init.js
@@ -10,8 +10,7 @@
     js: [
       window.SITE_CONFIG.cdnUrl + '/static/js/manifest.js',
       window.SITE_CONFIG.cdnUrl + '/static/js/vendor.js',
-      window.SITE_CONFIG.cdnUrl + '/static/js/app.js',
-      // window.SITE_CONFIG.cdnUrl + '/static/plugins/mock-1.0.0-beta3/mock-min.js'
+      window.SITE_CONFIG.cdnUrl + '/static/js/app.js'
     ]
   };
 
@@ -21,30 +20,49 @@
     _icon.setAttribute('rel', 'shortcut icon');
     _icon.setAttribute('type', 'image/x-icon');
     _icon.setAttribute('href', resList.icon);
-      document.getElementsByTagName('head')[0].appendChild(_icon);
+    document.getElementsByTagName('head')[0].appendChild(_icon);
   })();
 
   // 样式
   (function () {
-    var _script = null;
-    for (var i = 0; i < resList.css.length; i++) {
+    document.getElementsByTagName('html')[0].style.opacity = 0;
+    var i = 0;
+    var _style = null;
+    var createStyles = function () {
+      if (i >= resList.css.length) {
+        document.getElementsByTagName('html')[0].style.opacity = 1;
+        return;
+      }
       _style = document.createElement('link');
       _style.href = resList.css[i];
       _style.setAttribute('rel', 'stylesheet');
+      _style.onload = function () {
+        i++;
+        createStyles();
+      }
       document.getElementsByTagName('head')[0].appendChild(_style);
     }
+    createStyles();
   })();
 
   // 脚本
   document.onreadystatechange = function () {
-    if (document.readyState === 'complete') {
+    if (document.readyState === 'interactive') {
+      var i = 0;
       var _script = null;
-      for (var i = 0; i < resList.js.length; i++) {
+      var createScripts = function () {
+        if (i >= resList.js.length) {
+          return;
+        }
         _script = document.createElement('script');
         _script.src = resList.js[i];
-        _script.async = false
+        _script.onload = function () {
+          i++;
+          createScripts();
+        }
         document.getElementsByTagName('body')[0].appendChild(_script);
       }
+      createScripts();
     }
   };
 })();
