bc2df70584703ac0c8688c3be2afb5d51ceed93a
daxiongYang <daxiong.yang@qq.com>
2018-05-08 15:31:12 +0800

    修改左侧菜单icon-svg传null错误bug

--- src/views/main-sidebar-sub-menu.vue
+++ src/views/main-sidebar-sub-menu.vue
@@ -4,7 +4,7 @@
     :index="menu.menuId + ''"
     :popper-class="'site-sidebar--' + sidebarLayoutSkin + '-popper'">
     <template slot="title">
-      <icon-svg :name="menu.icon" class="site-sidebar__menu-icon"></icon-svg>
+      <icon-svg :name="menu.icon || ''" class="site-sidebar__menu-icon"></icon-svg>
       <span>{{ menu.name }}</span>
     </template>
     <sub-menu
@@ -15,7 +15,7 @@
     </sub-menu>
   </el-submenu>
   <el-menu-item v-else :index="menu.menuId + ''" @click="gotoRouteHandle(menu)">
-    <icon-svg :name="menu.icon" class="site-sidebar__menu-icon"></icon-svg>
+    <icon-svg :name="menu.icon || ''" class="site-sidebar__menu-icon"></icon-svg>
     <span>{{ menu.name }}</span>
   </el-menu-item>
 </template>
