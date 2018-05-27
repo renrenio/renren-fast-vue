fea8dd9f211e0f9d87c9495bca16fc530f7ebdb1
daxiongYang <daxiong.yang@qq.com>
2018-05-27 12:38:08 +0800

    注释错误 #23

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import { isURL } from '@/utils/validate'
 
 Vue.use(Router)
 
-// 开发环境不使用懒加载, 因为懒加载页面太多的话会造成webpack热更新太慢, 所以只有开发环境使用懒加载
+// 开发环境不使用懒加载, 因为懒加载页面太多的话会造成webpack热更新太慢, 所以只有生产环境使用懒加载
 const _import = require('./import-' + process.env.NODE_ENV)
 
 // 全局路由(无需嵌套上左右整体布局)
