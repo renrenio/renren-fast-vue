67d9fbac9a1196a61771866efce56336c522b9ed
daxiongYang <daxiong.yang@qq.com>
2018-05-27 11:09:37 +0800

    linux系统，引入主题色文件名大小写编译错误bug #22

--- src/element-ui-theme/index.js
+++ src/element-ui-theme/index.js
@@ -3,22 +3,23 @@
  *
  * tips:
  *  1. 此处只包含element-ui组件主题
- *  2. 要达到整站主题修改效果, 请确保[import './element-[#17b3a3]/index.css']当前主题色与[/src/assets/scss/_variables.scss]文件中[$--color-primary]属性值一致
+ *  2. 要达到整站主题修改效果, 请确保[active]当前主题色与[/src/assets/scss/_variables.scss]文件中[$--color-primary]属性值一致
  */
-import './element-#17b3a3/index.css' // 当前主题色
-export default {
+const theme = {
+  active: '#17b3a3', // 当前主题色
   list: [
-    '#3e8ef7', // 蓝色
-    '#997b71', // 棕色
     '#0bb2d4', // 青色
+    '#3e8ef7', // 蓝色
     '#11c26d', // 绿色
-    '#757575', // 灰色
+    '#17b3a3', // 蓝绿色
     '#667afa', // 靛青色
+    '#997b71', // 棕色
+    '#9463f7', // 紫色
+    '#757575', // 灰色
     '#eb6709', // 橙色
     '#f74584', // 粉红色
-    '#9463f7', // 紫色
-    '#ff4c52', // 红色
-    '#17b3a3', // 蓝绿色
-    '#fcb900'  // 黄色
+    '#fcb900', // 黄色
+    '#ff4c52'  // 红色
   ]
 }
+require(`./element-${theme.active.toUpperCase()}/index.css`)
