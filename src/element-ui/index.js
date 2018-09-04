909c8f18b7f856998e392e6766213eb242ea8824
daxiongYang <daxiong.yang@qq.com>
2018-09-04 14:54:52 +0800

    默认开放element-ui所有组件

--- src/element-ui/index.js
+++ src/element-ui/index.js
@@ -11,7 +11,7 @@ import Vue from 'vue'
 import {
   Pagination,
   Dialog,
-  // Autocomplete,
+  Autocomplete,
   Dropdown,
   DropdownMenu,
   DropdownItem,
@@ -23,25 +23,25 @@ import {
   InputNumber,
   Radio,
   RadioGroup,
-  // RadioButton,
+  RadioButton,
   Checkbox,
-  // CheckboxButton,
+  CheckboxButton,
   CheckboxGroup,
-  // Switch,
-  // Select,
-  // Option,
-  // OptionGroup,
+  Switch,
+  Select,
+  Option,
+  OptionGroup,
   Button,
-  // ButtonGroup,
+  ButtonGroup,
   Table,
   TableColumn,
-  // DatePicker,
-  // TimeSelect,
-  // TimePicker,
+  DatePicker,
+  TimeSelect,
+  TimePicker,
   Popover,
   Tooltip,
-  // Breadcrumb,
-  // BreadcrumbItem,
+  Breadcrumb,
+  BreadcrumbItem,
   Form,
   FormItem,
   Tabs,
@@ -49,38 +49,38 @@ import {
   Tag,
   Tree,
   Alert,
-  // Slider,
-  // Icon,
+  Slider,
+  Icon,
   Row,
   Col,
   Upload,
-  // Progress,
+  Progress,
   Badge,
   Card,
-  // Rate,
-  // Steps,
-  // Step,
-  // Carousel,
-  // CarouselItem,
-  // Collapse,
-  // CollapseItem,
-  // Cascader,
-  // ColorPicker,
-  // Transfer,
-  // Container,
-  // Header,
-  // Aside,
-  // Main,
-  // Footer,
+  Rate,
+  Steps,
+  Step,
+  Carousel,
+  CarouselItem,
+  Collapse,
+  CollapseItem,
+  Cascader,
+  ColorPicker,
+  Transfer,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
   Loading,
   MessageBox,
-  Message
-  // Notification
+  Message,
+  Notification
 } from 'element-ui'
 
 Vue.use(Pagination)
 Vue.use(Dialog)
-// Vue.use(Autocomplete)
+Vue.use(Autocomplete)
 Vue.use(Dropdown)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)
@@ -92,25 +92,25 @@ Vue.use(Input)
 Vue.use(InputNumber)
 Vue.use(Radio)
 Vue.use(RadioGroup)
-// Vue.use(RadioButton)
+Vue.use(RadioButton)
 Vue.use(Checkbox)
-// Vue.use(CheckboxButton)
+Vue.use(CheckboxButton)
 Vue.use(CheckboxGroup)
-// Vue.use(Switch)
-// Vue.use(Select)
-// Vue.use(Option)
-// Vue.use(OptionGroup)
+Vue.use(Switch)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(OptionGroup)
 Vue.use(Button)
-// Vue.use(ButtonGroup)
+Vue.use(ButtonGroup)
 Vue.use(Table)
 Vue.use(TableColumn)
-// Vue.use(DatePicker)
-// Vue.use(TimeSelect)
-// Vue.use(TimePicker)
+Vue.use(DatePicker)
+Vue.use(TimeSelect)
+Vue.use(TimePicker)
 Vue.use(Popover)
 Vue.use(Tooltip)
-// Vue.use(Breadcrumb)
-// Vue.use(BreadcrumbItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Tabs)
@@ -118,29 +118,29 @@ Vue.use(TabPane)
 Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Alert)
-// Vue.use(Slider)
-// Vue.use(Icon)
+Vue.use(Slider)
+Vue.use(Icon)
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Upload)
-// Vue.use(Progress)
+Vue.use(Progress)
 Vue.use(Badge)
 Vue.use(Card)
-// Vue.use(Rate)
-// Vue.use(Steps)
-// Vue.use(Step)
-// Vue.use(Carousel)
-// Vue.use(CarouselItem)
-// Vue.use(Collapse)
-// Vue.use(CollapseItem)
-// Vue.use(Cascader)
-// Vue.use(ColorPicker)
-// Vue.use(Transfer)
-// Vue.use(Container)
-// Vue.use(Header)
-// Vue.use(Aside)
-// Vue.use(Main)
-// Vue.use(Footer)
+Vue.use(Rate)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
+Vue.use(Collapse)
+Vue.use(CollapseItem)
+Vue.use(Cascader)
+Vue.use(ColorPicker)
+Vue.use(Transfer)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
 
 Vue.use(Loading.directive)
 
@@ -149,7 +149,7 @@ Vue.prototype.$msgbox = MessageBox
 Vue.prototype.$alert = MessageBox.alert
 Vue.prototype.$confirm = MessageBox.confirm
 Vue.prototype.$prompt = MessageBox.prompt
-// Vue.prototype.$notify = Notification
+Vue.prototype.$notify = Notification
 Vue.prototype.$message = Message
 
 Vue.prototype.$ELEMENT = { size: 'medium' }
