616e0748f3faef4d213b75400c455c2ff89d4a2e
云声 <36142424+kkwwang@users.noreply.github.com>
2018-12-12 16:41:46 +0800

    Update main.js
    添加element scrollbar组件

--- src/main.js
+++ src/main.js
@@ -10,7 +10,9 @@ import '@/assets/scss/index.scss'
 import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
 import { isAuth } from '@/utils'
 import cloneDeep from 'lodash/cloneDeep'
+import { Scrollbar } from 'element-ui'
 
+Vue.use(Scrollbar)
 Vue.use(VueCookie)
 Vue.config.productionTip = false
 
